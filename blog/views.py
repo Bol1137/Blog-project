b8920a3fdb46a1e243e5f116dcb3488904bc448d
Bol1137 <hdragon969@gmail.com>
2023-03-02 19:46:47 +0200

    Added cononical urls, and paginations

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.all()
+    # Pagination with 3 posts per page
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # if page_number is not ana integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
@@ -11,5 +22,4 @@ def post_detail(request, id):
    
     post = get_object_or_404(Post, id=id)    
     return render(request, 'blog/post/detail.html', {'post' : post})
-
 
